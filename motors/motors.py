c119abc4974e49c16d149093f681ea928e4a334b
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2024-12-29 15:04:57 +0100

    Correction

--- motors/motors.py
+++ motors/motors.py
@@ -1,14 +1,27 @@
-from collections import deque
 import controller
+from collections import deque
 from threading import Thread, Lock
-from time import sleep
 
-buffer = deque([])
-ctl = controller.Controller()
 
-def main():
-    while True:
-        if len(buffer) > 0:
-            speed_l, speed_r = buffer.popleft()
-            ctl.set_raw_motor_speed(speed_l, speed_r)
-        sleep(0.1) #On marque une pause pour éviter de trop consommer le CPU
+class Motors:
+    def __init__(self):
+        self.motor_buffer = deque([])
+        self.motor_buffer_lock = Lock()
+
+        self.motor_ctl = controller.Controller()
+        self.motor_ctl.set_motor_shutdown_timeout(5)
+
+        self.motor_thread = Thread(target=self.main)
+        self.motor_thread.run()
+
+    def main(self):
+        """
+        Donne une consigne au moteur
+        """
+        while True: 
+            with self.motor_buffer_lock:
+                if len(self.motor_buffer) > 0:
+                    speed_l, speed_r = self.motor_buffer.popleft()
+                else:
+                    speed_l, speed_r = 0, 0
+            self.motor_ctl.set_raw_motor_speed(speed_l, speed_r)
